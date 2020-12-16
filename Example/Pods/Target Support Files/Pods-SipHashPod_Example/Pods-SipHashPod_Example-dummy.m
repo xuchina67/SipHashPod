72af8ebfbce4d8628fdbc6f4f3f173b296a62b40
xuminjie <xuminjie@tron.network>
2020-12-16 16:54:55 +0800

    v0.1.0

---
+++ Example/Pods/Target Support Files/Pods-SipHashPod_Example/Pods-SipHashPod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SipHashPod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SipHashPod_Example
+@end
---
+++ Example/Pods/Target Support Files/SipHashPod/SipHashPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SipHashPod : NSObject
+@end
+@implementation PodsDummy_SipHashPod
+@end
